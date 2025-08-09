cb60c270c9775ec2f6722272a7275dd50e122cfd
Gerard <gerard.eklu@yahoo.fr>
2025-08-09 01:43:34 -0400

    New updates

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -10,20 +10,20 @@ export default function Contact() {
         </h2>
         <p className='text-lg mb-10'>
           I'm open to opportunities in DevOps, Full Stack, or Freelance work.
-          Feel free to reach out — whether it's a project idea, a job offer, or
+          Feel free to reach out whether it's a project idea, a job offer, or
           just to say hi!
         </p>
 
         <div className='flex justify-center gap-6 flex-wrap'>
-          {/* <a
-            href='mailto:youremail@example.com'
+          <a
+            href='mailto:eklugerard018@gmail.com'
             className='flex items-center gap-2 px-5 py-3 bg-[#F07050] text-white rounded hover:bg-orange-600 transition'
           >
             <MdEmail size={18} /> Email Me
-          </a> */}
+          </a>
 
           <a
-            href='https://github.com/yourusername'
+            href='https://github.com/gerardinhoo'
             target='_blank'
             className='flex items-center gap-2 px-5 py-3 bg-black text-white rounded hover:bg-gray-800 transition'
           >
@@ -31,7 +31,7 @@ export default function Contact() {
           </a>
 
           <a
-            href='https://linkedin.com/in/yourusername'
+            href='https://linkedin.com/in/gerard-eklu-270422151/'
             target='_blank'
             className='flex items-center gap-2 px-5 py-3 border border-[#F07050] text-[#F07050] rounded hover:bg-[#F07050] hover:text-white transition'
           >
